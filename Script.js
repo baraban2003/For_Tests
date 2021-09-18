6f0858ba854c44212198f555e845deff534ee7cb
Volodymyr <barabanv2003@gmail.com>
2021-09-18 13:13:23 +0300

    циклы + условия

--- Script.js
+++ Script.js
@@ -23,7 +23,7 @@
 
 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "Only Numbers");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -33,13 +33,46 @@ const personalMovieDB = {
     privat: false
 };
 
-const film = prompt('Один из последних просмотренных фильмов?', ""),
-    estimation = +prompt('На сколько оцените его?', "from 1 to 5"),
-    film2 = prompt('Один из последних просмотренных фильмов?', ""),
-    estimation2 = +prompt('На сколько оцените его?', "from 1 to 5");
+if (personalMovieDB.count < 10) {
+    alert ("Просмотрено довольно мало фильмов");
+ }
+ else if (personalMovieDB.count >=10 || personalMovieDB.count < 30) {
+     alert ("Вы классический зритель");
+ }
+ else if (personalMovieDB.count >= 10){
+     alert ("Вы Кинонаркоман");
+      }
+      else {alert ("Shit happen");}
 
-personalMovieDB.movies[film] = estimation;
-personalMovieDB.movies[film2] = estimation2;
+for (let i = 0; i < numberOfFilms; i++) {
+    const film = prompt('Один из последних просмотренных фильмов?', "");
+    const estimation = +prompt('На сколько оцените его?', "from 1 to 5");
+
+    if (film != null && estimation != null && film != '' && estimation != '' && film.length <= 50) {
+        personalMovieDB.movies[film] = estimation;
+        alert('you are Good!');
+    } else {
+        alert('Are you kidding ? ? ? ? ? Жопошник!!!!');
+        i--;
+    }
+}
+
 
 
 console.log(personalMovieDB);
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
